a5b976ece1bee7d1dfb8a5209ce4a9bf45953457
adarsh221156 <adarsh221156@gmail.com>
2022-01-11 02:28:00 +0530

    First and Final

---
+++ src/Coin.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import "./Coin.css";
+
+
+class Coin extends Component {
+    render(){
+        console.log(this.props.info.imgSrc);
+        return (
+        <div className='Coin'>
+        <img src={this.props.info.imgSrc} alt={this.props.info.side}/>
+        </div>
+        )
+    }
+}
+
+export default Coin;
